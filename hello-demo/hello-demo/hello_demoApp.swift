c94597fd4f10c0cf067102d36f85bf1dc58ea6e5
Claire Ho <torontoclaire@gmail.com>
2023-12-20 14:11:05 -0500

    Hello world

---
+++ hello-demo/hello-demo/hello_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_demoApp.swift
+//  hello-demo
+//
+//  Created by Chloe Gao on 2023-12-20.
+//
+
+import SwiftUI
+
+@main
+struct hello_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
